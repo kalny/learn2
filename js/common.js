dccf406258c9a14fe9ef18aee178d62fce5f4571
Anton Kalny <kalnyanton@gmail.com>
2017-02-09 08:59:18 +0200

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,54 @@
+$(function() {
+
+	$("a.modal-close").click(function(e){
+		e.preventDefault();
+
+		var btn = e.currentTarget;
+		var modal = $(btn).parents(".modal");
+
+		modal.fadeOut();
+	});
+
+	$("a.modal-button").click(function(e){
+		e.preventDefault();
+
+		var btn = e.currentTarget;
+		var modalID = $(btn).data("modal");
+
+		console.log(modalID);
+
+
+		$("#" + modalID).fadeIn();
+	});
+
+
+	jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+	});
+
+});
